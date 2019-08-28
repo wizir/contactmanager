e4c3795c5964686e08ec5416eafcebde24877b04
wojciechmizera <wizir@cyberdude.com>
2019-08-28 23:54:29 +0200

    Updates

---
+++ precache-manifest.8929fa8c483f78b3b958af87c5ef9c28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8847f92349467015a0de63445b95805",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "4b58d08b82d5d33ed470",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d4b65044d78a25b42e68",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "4b58d08b82d5d33ed470",
+    "url": "/contactmanager/static/js/2.da294201.chunk.js"
+  },
+  {
+    "revision": "d4b65044d78a25b42e68",
+    "url": "/contactmanager/static/js/main.8877e64f.chunk.js"
+  },
+  {
+    "revision": "93f1dd93a71eaf453ec4",
+    "url": "/contactmanager/static/js/runtime~main.b82d1e6e.js"
+  }
+]);
